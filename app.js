f535f63e31f52f3d789119f71d9b86d2535298b7
Leandro <73486095+leandro144@users.noreply.github.com>
2022-01-21 21:25:25 -0300

    five commit

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const nodemailer = require('nodemailer')
 const app = express();
+
 const process = require('process')
 const path = require('path')
+const db = require('./models/db')
 
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const res = require('express/lib/response');
 
 
 var handle = exphbs.create({
     defaultLayout: 'main'
-    });
+});
 
 
 app.use(express.static('public'));
@@ -20,31 +22,33 @@ app.set('view engine', 'handlebars');
 
 const port = process.env.PORT || 8080
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
 let login = "admin";
 let password = "12345";
 
-app.get('/login', (req,res) => {
+app.get('/login', (req, res) => {
 
-        res.sendFile(__dirname + '/public/login.html')
+    res.sendFile(__dirname + '/public/login.html')
 })
 
-app.get('/logado', (req,res) => {
+app.get('/logado', (req, res) => {
 
     res.sendFile(__dirname + '/public/logado.html')
 })
 
-app.post('/', (req,res) => {
-    if(req.body.password == password && req.body.login == login){
+app.post('/', (req, res) => {
+    if (req.body.password == password && req.body.login == login) {
         //logado com sucesso!//
 
         res.sendFile(__dirname + '/public/logado.html')
-    }else{
+    } else {
         res.sendFile(__dirname + '/public/login.html')
     }
-    
+
 })
 
 app.get('/cad', (req, res) => {
@@ -53,7 +57,7 @@ app.get('/cad', (req, res) => {
 
 app.post('/send', async (req, res) => {
 
-    const form = ("Nome: "+req.body.name+"\nE-mail: "+req.body.email+"\nTelefone: "+req.body.numbers)
+    const form = ("Nome: " + req.body.name + "\nE-mail: " + req.body.email + "\nTelefone: " + req.body.numbers)
 
     const transporter = nodemailer.createTransport({
         host: "smtp.live.com",
@@ -65,7 +69,7 @@ app.post('/send', async (req, res) => {
         }
     });
 
-   let message = await transporter.sendMail({ 
+    let message = await transporter.sendMail({
         from: "testecorretora123@outlook.com",
         to: "testecorretora123@outlook.com",
         text: form,
@@ -74,7 +78,7 @@ app.post('/send', async (req, res) => {
 
     if (message) {
         res.send("Formulário enviado com sucesso");
-    }else{
+    } else {
         res.send('Formulário não enviado')
     }
 
---
+++ models/db.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize("celke", "root", "", {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+sequelize.authenticate()
+.then(function(){
+    console.log("Conexão com banco de dados conectado com sucesso");
+}).catch(function(){
+     console.log("Erro: conexão com banco de dados não realizada com sucesso!");
+});
+
+module.exports = sequelize;
