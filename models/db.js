ed85362806a5a8ea54e6dafe09645718ad1f6c3c
Leandro <73486095+leandro144@users.noreply.github.com>
2022-01-29 17:31:17 -0300

    commit

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@
 
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('celke', 'root', '', {
+const sequelize = new Sequelize('celke', 'root', '' , {
     host: 'localhost',
     dialect: 'mysql',
     port: '3306',
